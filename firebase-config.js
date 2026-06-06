b6af1ef0a1542d7f7b6634370568bcb0cec76b3f
dayanandkendre <dayanandkendre10@gmail.com>
2026-06-06 13:55:18 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyAXzw_g1r7kvYC2d6_d4RqDOoTF_svAphc",
+
+  authDomain: "typenext-5bd90.firebaseapp.com",
+
+  projectId: "typenext-5bd90",
+
+  storageBucket: "typenext-5bd90.firebasestorage.app",
+
+  messagingSenderId: "848488048236",
+
+  appId: "1:848488048236:web:a742a647977a48ca63da49",
+
+  measurementId: "G-B5WGZM350T"
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
---
+++ login.js
@@ -0,0 +1,92 @@
+import { auth } from "./firebase-config.js";
+
+import {
+GoogleAuthProvider,
+signInWithPopup,
+onAuthStateChanged,
+signOut
+}
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+
+const provider =
+new GoogleAuthProvider();
+
+
+document
+.getElementById(
+"googleLoginBtn"
+)
+.addEventListener(
+"click",
+async function(){
+
+try{
+
+const result =
+await signInWithPopup(
+auth,
+provider
+);
+
+const user =
+result.user;
+
+localStorage.setItem(
+"userName",
+user.displayName
+);
+
+localStorage.setItem(
+"userEmail",
+user.email
+);
+
+localStorage.setItem(
+"userPhoto",
+user.photoURL
+);
+
+document
+.getElementById(
+"loginModal"
+)
+style.display =
+"none";
+
+alert(
+"Welcome " +
+user.displayName
+);
+
+}
+catch(error){
+
+console.error(
+error
+);
+
+alert(
+"Login Failed"
+);
+
+}
+
+});
+
+
+onAuthStateChanged(
+auth,
+(user)=>{
+
+if(user){
+
+console.log(
+"Logged In:",
+user.displayName
+);
+
+}
+
+}
+);
