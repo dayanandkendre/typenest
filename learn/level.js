c59d596bcd323a71caccee834692c98cd42079ed
dayanandkendre <dayanandkendre10@gmail.com>
2026-06-07 14:38:50 +0530

    Update level.js

--- learn/level.js
+++ learn/level.js
@@ -606,6 +606,43 @@ if(level > streak){
 
 }
 
+const userUID =
+localStorage.getItem(
+"userUID"
+);
+
+if(userUID){
+
+await setDoc(
+doc(
+db,
+"users",
+userUID
+),
+{
+currentLevel:
+localStorage.getItem(
+"currentLevel"
+),
+
+sectionUnlocked:
+localStorage.getItem(
+"sectionUnlocked"
+),
+
+bestStreak:
+localStorage.getItem(
+"bestStreak"
+)
+
+},
+{
+merge:true
+}
+);
+
+}
+      
 document
 .getElementById(
 "popup"
