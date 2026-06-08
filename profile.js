cf4b4ad889a48e264c2359d1a45b1e8a06e5bae9
dayanandkendre <dayanandkendre10@gmail.com>
2026-06-09 00:18:03 +0530

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,61 @@
+import { auth } from "./firebase-config.js";
+
+import {
+
+    onAuthStateChanged,
+    signOut
+
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+const userName =
+document.getElementById("userName");
+
+const userEmail =
+document.getElementById("userEmail");
+
+const profilePhoto =
+document.getElementById("profilePhoto");
+
+const logoutBtn =
+document.getElementById("logoutBtn");
+
+onAuthStateChanged(auth,(user)=>{
+
+    if(!user){
+
+        window.location.href =
+        "login.html";
+
+        return;
+
+    }
+
+    userName.textContent =
+    user.displayName ||
+    "TypeNest User";
+
+    userEmail.textContent =
+    user.email;
+
+    if(user.photoURL){
+
+        profilePhoto.src =
+        user.photoURL;
+
+    }
+
+});
+
+logoutBtn.addEventListener(
+"click",
+()=>{
+
+    signOut(auth)
+    .then(()=>{
+
+        window.location.href =
+        "login.html";
+
+    });
+
+});
