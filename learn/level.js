5c4a3a3596b924e9d3808115eb25ba6af73aeaae
dayanandkendre <dayanandkendre10@gmail.com>
2026-06-11 00:00:52 +0530

    Update level.js

--- learn/level.js
+++ learn/level.js
@@ -203,6 +203,11 @@ lettersHTML;
 const input =
 document.getElementById("typingInput");
 
+const keySound =
+document.getElementById(
+"keySound"
+);
+
 let current = 0;
 let mistakes = 0;
 
@@ -673,6 +678,14 @@ function(){
 document.addEventListener(
 "keydown",
 function(event){
+
+   if(keySound){
+
+    keySound.currentTime = 0;
+
+    keySound.play().catch(()=>{});
+
+}
 
     if(event.getModifierState("CapsLock")){
 
