0a331f7462ff6fc6b814d0d62f49fbadc1e75a76
dayanandkendre <dayanandkendre10@gmail.com>
2026-06-11 10:43:03 +0530

    Update typing.js

--- typing.js
+++ typing.js
@@ -1,4 +1,12 @@
 import { db } from "./firebase-config.js";
+
+import {
+doc,
+getDoc,
+updateDoc
+}
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
 /* =========================================
    PARAGRAPHS
 ========================================= */
