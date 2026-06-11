d7165d1ce9a4964b7e20f29a6df8ac7fb476f503
dayanandkendre <dayanandkendre10@gmail.com>
2026-06-11 16:40:14 +0530

    Update leaderboard.js

--- leaderboard.js
+++ leaderboard.js
@@ -55,3 +55,35 @@ console.log(
 users
 );
 
+leaderboardBox.innerHTML = "";
+
+users.forEach(
+function(user,index){
+
+leaderboardBox.innerHTML +=
+
+<div class="leader-row">
+
+<div class="rank">
+
+#${index + 1}
+
+</div>
+
+<div class="user">
+
+${user.name || "User"}
+
+</div>
+
+<div class="wpm">
+
+${user.bestWpm || 0} WPM
+
+</div>
+
+</div>
+;
+
+});
+
