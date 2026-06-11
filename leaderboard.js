5519fb3cb87975f82bb63535e64881c564e6b27d
dayanandkendre <dayanandkendre10@gmail.com>
2026-06-11 16:45:26 +0530

    Update leaderboard.js

--- leaderboard.js
+++ leaderboard.js
@@ -57,33 +57,24 @@ users
 
 leaderboardBox.innerHTML = "";
 
-users.forEach(
-function(user,index){
+users.forEach(function(user,index){
 
-leaderboardBox.innerHTML +=
-
+leaderboardBox.innerHTML += `
 <div class="leader-row">
-
 <div class="rank">
-
 #${index + 1}
-
 </div>
 
 <div class="user">
-
 ${user.name || "User"}
-
 </div>
 
 <div class="wpm">
-
 ${user.bestWpm || 0} WPM
-
 </div>
 
 </div>
-;
+`;
 
 });
 
