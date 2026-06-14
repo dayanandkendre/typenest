c0911afbca8a1d286c993b1ffb36839bf70a2461
dayanandkendre <dayanandkendre10@gmail.com>
2026-06-14 11:48:04 +0530

    Update certificate.js

--- certificate.js
+++ certificate.js
@@ -1,4 +1,15 @@
 import {
+doc,
+getDoc
+}
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+import {
+db
+}
+from "./firebase-config.js";
+
+import {
 auth
 }
 from "./firebase-config.js";
@@ -20,12 +31,77 @@ document.getElementById(
 
 onAuthStateChanged(
 auth,
-(user)=>{
+async(user)=>{
 
 if(!user){
 
 window.location.href =
 "login.html";
+
+return;
+
+}
+
+const userRef =
+doc(
+db,
+"users",
+user.uid
+);
+
+const userSnap =
+await getDoc(
+userRef
+);
+
+if(!userSnap.exists()){
+
+window.location.href =
+"profile.html";
+
+return;
+
+}
+
+const data =
+userSnap.data();
+
+const home =
+data.progress?.home || 1;
+
+const bottom =
+data.progress?.bottomrow || 1;
+
+const words =
+data.progress?.words || 1;
+
+const numbers =
+data.progress?.numbers || 1;
+
+const advanced =
+data.progress?.advanced || 1;
+
+const totalCompleted =
+
+(home - 1) +
+(bottom - 1) +
+(words - 1) +
+(numbers - 1) +
+(advanced - 1);
+
+const overallProgress =
+Math.round(
+(totalCompleted / 100) * 100
+);
+
+if(overallProgress < 100){
+
+alert(
+"Complete all levels to unlock certificate."
+);
+
+window.location.href =
+"profile.html";
 
 return;
 
