0d597785f093cc44d959cbd1fc2b39cf8f794dd3
dayanandkendre <dayanandkendre10@gmail.com>
2026-06-21 14:59:50 +0530

    Update toprow.js

--- learn/toprow.js
+++ learn/toprow.js
@@ -611,12 +611,12 @@ else{
 }
 
 localStorage.setItem(
-"level" + level + "Stars",
+"topLevel" + level + "Stars",
 stars
 );
 
 localStorage.setItem(
-"level" + level + "Score",
+"topLevel" + level + "Score",
 accuracy + "%"
 );
 
