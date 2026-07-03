7aed231071ceb32d42770514d5066abd635330c1
dayanandkendre <dayanandkendre10@gmail.com>
2026-07-03 18:57:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -498,3 +498,32 @@ menuBtn.innerHTML = "☰";
 
 });
 
+
+
+/* =========================================
+   FAQ ACCORDION
+========================================= */
+
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach(item => {
+
+    const btn = item.querySelector(".faq-question");
+
+    btn.addEventListener("click", () => {
+
+        faqItems.forEach(other => {
+
+            if(other !== item){
+
+                other.classList.remove("active");
+
+            }
+
+        });
+
+        item.classList.toggle("active");
+
+    });
+
+});
